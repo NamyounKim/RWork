260a281335ec67ad3bc7dd59198ed742d0a209f4
namyun <namyun84@gmail.com>
2017-09-24 19:04:05 +0900

    9_24_win

--- FastCampus/Week_2/data_visualization.R
+++ FastCampus/Week_2/data_visualization.R
@@ -9,30 +9,30 @@ library(MASS)
 data("Cars93")
 Cars93
 
-##1. Bar Chart
-ggplot(Cars93, aes(x=Type)) + geom_bar(stat = "count")
-ggplot(Cars93, aes(x=Type)) + geom_bar(stat = "count") + labs(x="Car Type") + theme_minimal()
+## 1. Bar Chart
+ggplot(Cars93, aes(x=Type)) + geom_bar(stat = "count") # 막대의 높이를 빈도수로 할 경우
+ggplot(Cars93, aes(x=Type)) + geom_bar(stat = "count") + labs(x="Car Type") # X축에 이름 붙이기
 ggplot(Cars93, aes(x=Type)) + geom_bar(stat = "count") + labs(x="Car Type") + 
   ggtitle("The Count by Car Type") + theme(plot.title = element_text(hjust = 0.5, size = 25)) # 차트에 제목 넣기
 
-##2. Stack Bar Chart
-ggplot(Cars93, aes(x=Type, fill=Origin)) + geom_bar(stat = "count")
-ggplot(Cars93, aes(x=Type, fill=Origin)) + geom_bar(stat = "count", position = "dodge") # Stack 하지 않기
+## 2. Stack Bar Chart
+ggplot(Cars93, aes(x=Type, fill=AirBags)) + geom_bar(stat = "count")
+ggplot(Cars93, aes(x=Type, fill=AirBags)) + geom_bar(stat = "count", position = "dodge") # Stack 하지 않기
 
 #type별 가격 평균값 구한 후 바차트 그리기
-mpg_temp = Cars93 %>% group_by(Type) %>% summarise(avgPrice = mean(Price))
-ggplot(mpg_temp, aes(x=Type, y=avgPrice)) + geom_bar(stat = "identity") #데이터셋 값 그대로
+temp_set = Cars93 %>% group_by(Type) %>% summarise(avgPrice = mean(Price))
+ggplot(temp_set, aes(x=Type, y=avgPrice)) + geom_bar(stat = "identity") #막대의 높이를 값으로 할 경우
 
 #비율 누적 Bar Chart 구하기
 #자동차 type별 Origin 비율구한 후 비율 누적 Bar Chart 그리기
-df2 = Cars93 %>% group_by(Type, Origin) %>% summarise(n=n()) %>% 
+temp_set = Cars93 %>% group_by(Type, AirBags) %>% summarise(n=n()) %>% 
   mutate(ratio = n/sum(n))
 
-ggplot(df3, aes(x=Type, y=ratio, fill=Origin)) + geom_bar(stat = "identity")
+ggplot(temp_set, aes(x=Type, y=ratio, fill=AirBags)) + geom_bar(stat = "identity")
 
 
-##3. Scatter Plot
-ggplot(Cars93, aes(x=MPG.city, y=Price)) + geom_point(stat = "identity")
+## 3. Scatter Plot
+ggplot(Cars93, aes(x=MPG.city, y=Price)) + geom_point()
 ggplot(Cars93, aes(x=MPG.city, y=Price)) + geom_point(shape=9) # 점모양 바꾸기
 ggplot(Cars93, aes(x=MPG.city, y=Price, label = Manufacturer)) + geom_text(size=3) # 점 대신 라벨값 보여주기
 ggplot(Cars93, aes(x=MPG.city, y=Price)) + geom_point(shape=2) + geom_smooth(method=lm) # 선형식 그리기
@@ -40,30 +40,30 @@ ggplot(Cars93, aes(x=MPG.city, y=Price)) + geom_point(shape=2) + geom_smooth() #
 ggplot(Cars93, aes(x=MPG.city, y=Price, color=factor(Manufacturer))) + geom_point(shape=2) #특정 범주형 값 별로 색깔 다르게 하기
 ggplot(Cars93, aes(x=MPG.city, y=Price, color=factor(Manufacturer), size=EngineSize)) + geom_point(shape=18) #특정 범주형 값 별로 색깔/크기 다르게 하기
 
-##4. Box Plot
-ggplot(Cars93, aes(x=factor(Manufacturer), y=Price)) + geom_boxplot() + theme(axis.text.x=element_text(angle = 45, hjust = 1))
-ggplot(Cars93, aes(x=factor(Type), y=Price, fill=factor(Origin))) + geom_boxplot()
+## 4. Box Plot
+ggplot(Cars93, aes(x=factor(AirBags), y=Price)) + geom_boxplot() + theme(axis.text.x=element_text(angle = 45, hjust = 1))
+ggplot(Cars93, aes(x=factor(Type), y=Price, fill=factor(AirBags))) + geom_boxplot()
 
 #샘플 데이터 가져오기
 data("economics")
 economics
 
-##5. Line Chart
-ggplot(economics, aes(x=date, y=pop)) + geom_line(stat = "identity")
+## 5. Line Chart
+ggplot(economics, aes(x=date, y=pop)) + geom_line()
 
 ggplot(economics, aes(x=pop, y=unemploy)) + geom_point()
 
 #축 눈금 간격 조절하기
 ggplot(economics, aes(x=pop, y=unemploy)) + geom_point() + 
-  scale_x_continuous(breaks = seq(200000, 330000, 10000)) + 
-  theme(axis.text.x=element_text(angle = 45, hjust = 1))
+  scale_x_continuous(breaks = seq(200000, 330000, 10000)) + # 20만 ~ 33만 사이를 만단위로 나눠서 X축에 표시하기
+  theme(axis.text.x=element_text(angle = 45, hjust = 1))    # X축 텍스트를 45도 기울여서 표시하기
 
 #연도별로 색깔 다르게해서 보여주기
 economics$year = substr(economics$date,1,4)
 ggplot(economics, aes(x=pop, y=unemploy, color=factor(year))) + geom_point()
 
 
-##6. 상관계수행렬 그리기
+## 6. 상관계수행렬 그리기
 subCars93 = Cars93 %>% dplyr::select(Price, MPG.city, MPG.highway, EngineSize)
 plot(subCars93)
 
@@ -75,5 +75,5 @@ install.packages("corrplot")
 library(corrplot)
 corrplot(corDf)
 corrplot(corDf, method = "shade", tl.srt=45)
-corrplot(corDf, method = "shade", tl.srt=45, addCoef.col = "black", order="AOE")
-corrplot(corDf, method = "shade", tl.srt=45, addCoef.col = "black", order="AOE", type = "lower")
+corrplot(corDf, method = "shade", tl.srt=45, addCoef.col = "black")
+corrplot(corDf, method = "shade", tl.srt=45, addCoef.col = "black", type = "lower")
