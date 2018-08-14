92f85f3ead9640d99051b6f8d1dcb5e4ae7e2110
namyun <namyun84@gmail.com>
2018-08-14 11:04:36 +0900

    from_pc_company
    from_pc_company

--- Crawler/청화대_청원글_수집.R
+++ Crawler/청화대_청원글_수집.R
@@ -3,10 +3,13 @@ library(rvest)
 library(stringr)
 library(readr)
 library(xml2)
+library(tidyr)
+library(dplyr)
+library(data.table)
 
 # List 가져오기
-petitionList = data.frame(link=as.character(), category=as.character())
-for(i in 1:2){
+petitionList = data.table(link=as.character(), category=as.character(), agree_count = as.numeric())
+for(i in 1:11000){
   Sys.sleep(runif(1,1,5))
   print(i)
   url = paste0("https://www1.president.go.kr/petitions?only=finished&page=",i)
@@ -17,15 +20,17 @@ for(i in 1:2){
   #link = link[2:length(link)]
   category = read_url_result %>% html_nodes(xpath = "//*[@id=\"cont_view\"]/div/div/div[1]/div/div/div[2]") %>% html_nodes("div.bl_category.cs") %>% html_text()
   
+  agree_count = read_url_result %>% html_nodes(xpath = "//*[@id=\"cont_view\"]/div/div/div[1]/div/div/div[2]") %>% html_nodes("div.bl_agree.cb") %>% html_text()
+  agree_count = str_replace_all(trimws(agree_count),pattern = "참여인원 ", replacement = "")
+  agree_count = str_replace_all(agree_count, pattern = "명", replacement = "")
+  agree_count = as.numeric(agree_count)
   
-  agree_count = read_url_result %>% html_nodes(xpath = "//*[@id=\"cont_view\"]/div/div/div[1]/div/div/div[2]") %>% html_node("div.bl_agree.cb") %>% html_text()
-  
-  rows = data.frame(link, category, stringsAsFactors = F)
+  rows = data.frame(link, category, agree_count, stringsAsFactors = F)
   petitionList = rbind(petitionList, rows)
 }
 
 # content 가져오기
-petitions = data.frame(title=as.character(), content=as.character(), startDate=as.character(), endDate=as.character(), agreeCount=as.numeric())
+petition_content = data.frame(title=as.character(), content=as.character(), startDate=as.character(), endDate=as.character(), agreeCount=as.numeric())
 for(i in 1:nrow(petitionList)){
 #for(i in 1:10){
   print(i)
@@ -40,6 +45,7 @@ for(i in 1:nrow(petitionList)){
   # 내용 가져오기
   content = read_html_result %>% html_nodes("div.View_write") %>% html_text()
   content = trimws(content)
+  content = str_replace_all(content, pattern = "[\t\r\n]", replacement = "")
   
   # 날짜 가져오기
   startDate = read_html_result %>% html_nodes(xpath = "//*[@id=\"cont_view\"]/div/div[1]/div/div[1]/div/div[2]/ul/li[2]/text()") %>% html_text()
@@ -51,7 +57,7 @@ for(i in 1:nrow(petitionList)){
   agreeCount = as.numeric(agreeCount)
   
   row = data.frame(title, content, startDate, endDate, agreeCount, stringsAsFactors = F)
-  petitions = rbind(petitions, row)
+  petition_content = rbind(petition_content, row)
   Sys.sleep(runif(1,1,5))
 }
 
