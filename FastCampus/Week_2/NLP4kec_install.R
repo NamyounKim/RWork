18bc816e8d7473fad84dccf8c8c2a2b5efe12203
namyun <namyun84@gmail.com>
2018-12-31 10:43:35 +0900

    company
    company

--- FastCampus/Week_2/NLP4kec_install.R
+++ FastCampus/Week_2/NLP4kec_install.R
@@ -19,8 +19,8 @@ install.packages("본인PC의 경로/NLP4kec_1.2.0.tgz", repos = NULL, type = .P
 library(NLP4kec)
 
 # 안될 경우 아래 변수 세팅 실행 후 다시 설치
-Sys.setenv(JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_121.jdk/Contents/Home/jre") # 본인 PC경로로 세팅해준다.
-dyn.load('/Library/Java/JavaVirtualMachines/jdk1.8.0_121.jdk/Contents/Home/jre/lib/server/libjvm.dylib') # 본인 PC경로로 세팅해준다.
+#Sys.setenv(JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_191.jdk/Contents/Home/jre") # 본인 PC경로로 세팅해준다.
+#dyn.load('/Library/Java/JavaVirtualMachines/jdk1.8.0_191.jdk/Contents/Home/jre/lib/server/libjvm.dylib') # 본인 PC경로로 세팅해준다.
 
 
 
